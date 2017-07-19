b90ea999d5c7aa192400c618ca635173479b8eb9
Shaojung <shaojung@gmail.com>
2017-07-19 11:39:19 +0800

    display string in listview

--- app/src/main/java/com/example/yvtc/yvtc071902/MainActivity.java
+++ app/src/main/java/com/example/yvtc/yvtc071902/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.yvtc.yvtc071902;
 
+import android.Manifest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -10,7 +13,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     ListView lv;
-    String cities[] = {"台北", "台中", "台南", "高雄"};
+    String cities[] = {"台北", "台中", "台南", "高雄","台北1", "台中1", "台南1", "高雄1","台北2", "台中2", "台南2", "高雄2"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public int getCount() {
-            return 5;
+            return cities.length;
         }
 
         @Override
@@ -40,9 +43,14 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            TextView tv = new TextView(MainActivity.this);
-            tv.setText("Hello:" + position);
-            return tv;
+            Log.d("MyView", "getView position:" + position);
+
+            LayoutInflater inflater = LayoutInflater.from(MainActivity.this);
+            View v = inflater.inflate(R.layout.myitem, null);
+
+            TextView tv = (TextView) v.findViewById(R.id.textView);
+            tv.setText(cities[position]);
+            return v;
         }
     }
 
